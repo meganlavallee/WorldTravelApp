742742934a2b39f211ef192a7a35e1ff5bb13344
arjunsingh <arjunksingh1027@gmail.com>
2021-01-21 12:41:42 -0700

    mongodb

--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,18 @@
+// dependencies
 const express = require("express");
 const morgan = require("morgan");
 const helmet = require("helmet");
 const cors = require("cors");
+const mongoose = require("mongoose");
 
+// dotenve
+require("dotenv").config();
+
+// middleware
 const middlewares = require("./middlewares");
+
+// mongoose connection
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
 
 const app = express();
 
@@ -11,18 +20,20 @@ const app = express();
 app.use(morgan("common"));
 app.use(helmet());
 app.use(cors({
-    origin: "http://localhost:3000"
+    origin: process.env.CORS_ORIGIN
 }));
 
 app.use(middlewares.notFound);
 app.use(middlewares.errorHandler);
 
+// api get route
 app.get("/", (req, res) => {
     res.json({
         message: "hello world"
     })
 });
 
+// listening on port
 const port = process.env.PORT || 3000
 app.listen(port, () => {
     console.log(`listening on http://localhost:${port}`)
