69d7546228fe8f96e04686ee2474d396ad7da000
arjunsingh <arjunksingh1027@gmail.com>
2021-01-21 20:26:44 -0700

    working on api calls

--- server/src/middlewares.js
+++ server/src/middlewares.js
@@ -1,19 +1,19 @@
 const notFound = (req, res, next) => {
-    const error = new Error (`not found - ${req.originalUrl}`);
+    const error = new Error(`Not Found - ${req.originalUrl}`);
     res.status(404);
     next(error);
-};
-
-const errorHandler = (error, req, res, next) => {
+  };
+  
+  const errorHandler = (error, req, res, next) => {
     const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
     res.status(statusCode);
     res.json({
-        message: error.message,
-        stack: process.env.NODE_ENV === "production" ? "error" : error.stack,  
+      message: error.message,
+      stack: process.env.NODE_ENV === 'production' ? '🥞' : error.stack,
     });
-}
-
-module.exports = {
+  };
+  
+  module.exports = {
     notFound,
-    errorHandler
-};
+    errorHandler,
+  };
