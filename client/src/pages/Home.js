6826dccb685d1fae5dc8d3eb918afd10c45499f2
Sergey Lugovoy <serg.lugovoy.81@gmail.com>
2021-01-22 11:10:19 -0700

    revealed token

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -6,16 +6,17 @@ export default function Home() {
   const [viewport, setViewport] = useState({
     width: "100vw",
     height: "100vw",
-    latitude: 37.7577,
-    longitude: -122.4376,
+    latitude: 37.6,
+    longitude: -95.665,
     zoom: 3
   });
+  const REACT_APP_MAPBOX_TOKEN = 'pk.eyJ1Ijoic2x1Z292b3k4MSIsImEiOiJja2s3Yjk3d20wYzhoMnhtaXo0N3MxZnRoIn0.iRRYqUDtPqDSubEVG9RSgw';
 
   return (
     <ReactMapGL
       {...viewport}
       mapStyle="mapbox://styles/slugovoy81/ckk7ccpwo098t18qyk7ydw402"
-      mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+      mapboxApiAccessToken={REACT_APP_MAPBOX_TOKEN}
       onViewportChange={nextViewport => setViewport(nextViewport)}
     />
   );
