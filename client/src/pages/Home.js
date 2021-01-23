6ca82bcf506ea2e782bac2c06259014b1f1bcc53
Sergey Lugovoy <serg.lugovoy.81@gmail.com>
2021-01-22 20:41:25 -0700

    added markers, styles

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -4,6 +4,7 @@ import entriesList from "../utils/api";
 
 export default function Home() {
   const [showPopup, setPopup] = useState({});
+  const [newLocation, setNewLocation] = useState(null);
   const [entries, setEntries] = useState([]);
   const [viewport, setViewport] = useState({
     width: "100vw",
@@ -21,8 +22,12 @@ export default function Home() {
   }, []);
 
   const showNewMarker = (e) => {
-    console.log(e);
-  }
+    const [longitude, latitude] = e.lngLat;
+    setNewLocation({
+      latitude,
+      longitude,
+    });
+  };
 
   // console.log(entries);
   const REACT_APP_MAPBOX_TOKEN =
@@ -43,6 +48,7 @@ export default function Home() {
             key={entry._id}
             latitude={entry.latitude}
             longitude={entry.longitude}
+          
           >
             <div
               onClick={() =>
@@ -52,11 +58,11 @@ export default function Home() {
               }
             >
               <svg
-                className="marker"
+                className="marker red"
                 style={{
-                  width: `${6 * viewport.zoom}px`,
-                  height: `${6 * viewport.zoom}px`,
-                  stroke: "#f8c102",
+                  width: `${5 * viewport.zoom}px`,
+                  height: `${5 * viewport.zoom}px`,
+                  stroke: "green",
                 }}
                 viewBox="0 0 24 24"
                 strokeWidth="2"
@@ -91,6 +97,46 @@ export default function Home() {
           ) : null}
         </>
       ))}
+      {newLocation ? (
+        <>
+          <Marker
+            latitude={newLocation.latitude}
+            longitude={newLocation.longitude}
+          >
+            <div>
+              <svg
+                className="marker blue"
+                style={{
+                  width: `${5 * viewport.zoom}px`,
+                  height: `${5 * viewport.zoom}px`,
+                  stroke: "white",
+                }}
+                viewBox="0 0 24 24"
+                strokeWidth="2"
+                fill="none"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"></path>
+                <circle cx="12" cy="10" r="3"></circle>
+              </svg>
+            </div>
+          </Marker>
+          <Popup
+            latitude={newLocation.latitude}
+            longitude={newLocation.longitude}
+            closeButton={true}
+            closeOnClick={false}
+            dynamicPosition={true}
+            onClose={() => setNewLocation(null)}
+            anchor="top"
+          >
+            <div className="popup">
+              <h3>Add your info here!</h3>
+            </div>
+          </Popup>
+        </>
+      ) : null}
     </ReactMapGL>
   );
 }
