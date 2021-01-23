13f1d474f31b6fdbc8a19390ed3f5be0c1914d39
Sergey Lugovoy <serg.lugovoy.81@gmail.com>
2021-01-23 14:57:47 -0700

    create new works, need fix seeds

--- server/src/seeders/seeds.js
+++ server/src/seeders/seeds.js
@@ -1,22 +1,22 @@
 // Packages and variables
 let mongoose = require("mongoose");
-let db = require("../models/LogEntry");
+let LogEntry = require("../models/LogEntry");
 
 // Connection for Heroku and MongoDB Atlas
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/travelapp", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-});
-
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/travelapp", {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+//   useCreateIndex: true,
+//   useFindAndModify: false,
+// });
+async function seeds() { 
 let seed = [
   {
     title: "Albuquerque Zoo",
     description: "Something",
     rating: 5,
     latitude: 35.0781,
-    longitude: 106.6632,
+    longitude: -106.6632,
     visitDate: "2021-01-22T23:33:27.415Z",
 
   },
@@ -26,7 +26,7 @@ let seed = [
     description: "Something",
     rating: 5,
     latitude: 39.7392,
-    longitude: 104.9903,
+    longitude: -104.9903,
     visitDate: "2021-01-22T23:33:27.415Z",
 
   },
@@ -36,19 +36,14 @@ let seed = [
     description: "Something",
     rating: 5,
     latitude: 38.5816,
-    longitude: 121.4944,
+    longitude: -121.4944,
     visitDate: "2021-01-22T23:33:27.415Z",
 
   }
 ]
 
-
-
-
-
-
-db.LogEntry.deleteMany({})
-  .then(() => db.LogEntry.collection.insertMany(seed))
+ await LogEntry.deleteMany({})
+  .then(() => LogEntry.collection.insertMany(seed))
   .then(data => {
     console.log(data.result.n + " records inserted!");
     process.exit(0);
@@ -57,3 +52,6 @@ db.LogEntry.deleteMany({})
     console.error(err);
     process.exit(1);
   });
+}
+
+  module.exports = seeds();
