e64c8945b55e97a822376ad4d0c729f9926756aa
Sergey Lugovoy <serg.lugovoy.81@gmail.com>
2021-01-23 21:58:38 -0700

    finished basic, separated components

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from "react";
-import ReactMapGL, { Marker, Popup } from "react-map-gl";
+import ReactMapGL from "react-map-gl";
 import entriesList from "../utils/api";
-import NewLocationForm from "../components/NewLocationForm"
+// import NewLocationForm from "../components/NewLocationForm"
+import OldMarkers from "../components/OldMarkers"
+import NewMarkers from "../components/NewMarkers"
 
 export default function Home() {
   const [showPopup, setPopup] = useState({});
@@ -48,105 +50,10 @@ export default function Home() {
       onDblClick={showNewMarker}
     >
       {entries.map((entry) => (
-        <React.Fragment key={entry._id}>
-          <Marker
-            latitude={entry.latitude}
-            longitude={entry.longitude}
-          
-          >
-            <div
-              onClick={() =>
-                setPopup({
-                  [entry._id]: true,
-                })
-              }
-            >
-              <svg
-                className="marker red"
-                style={{
-                  width: `${5 * viewport.zoom}px`,
-                  height: `${5 * viewport.zoom}px`,
-                  stroke: "green",
-                }}
-                viewBox="0 0 24 24"
-                strokeWidth="2"
-                fill="none"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              >
-                <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"></path>
-                <circle cx="12" cy="10" r="3"></circle>
-              </svg>
-            </div>
-          </Marker>
-          {showPopup[entry._id] ? (
-            <Popup
-              latitude={entry.latitude}
-              longitude={entry.longitude}
-              closeButton={true}
-              closeOnClick={false}
-              dynamicPosition={true}
-              onClose={() => setPopup({})}
-              anchor="top"
-            >
-              <div className="popup">
-                <h3>{entry.title}</h3>
-                <p>Comments: {entry.comments}</p>
-                <p>Description: {entry.description}</p>
-                <small>
-                  Visited on: {new Date(entry.visitDate).toLocaleDateString()}
-                </small>
-                <p>Rating: {entry.rating}</p>
-                {entry.image && <img src={entry.image} alt={entry.title} />}
-              </div>
-            </Popup>
-          ) : null}
-        </React.Fragment>
+        <OldMarkers  key={entry._id} entry={entry} setPopup={setPopup} showPopup={showPopup} viewport={viewport}/>
       ))}
       {newLocation ? (
-        <>
-          <Marker
-            latitude={newLocation.latitude}
-            longitude={newLocation.longitude}
-          >
-            <div>
-              <svg
-                className="marker blue"
-                style={{
-                  width: `${5 * viewport.zoom}px`,
-                  height: `${5 * viewport.zoom}px`,
-                  stroke: "white",
-                }}
-                viewBox="0 0 24 24"
-                strokeWidth="2"
-                fill="none"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              >
-                <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"></path>
-                <circle cx="12" cy="10" r="3"></circle>
-              </svg>
-            </div>
-          </Marker>
-          <Popup
-            latitude={newLocation.latitude}
-            longitude={newLocation.longitude}
-            closeButton={true}
-            closeOnClick={false}
-            dynamicPosition={true}
-            onClose={() => setNewLocation(null)}
-            anchor="top"
-          >
-            <div className="popup">
-              <NewLocationForm
-              onClose={() => {
-                setNewLocation(null);
-                getAllPlaces();
-              }} 
-              location={newLocation}/>
-            </div>
-          </Popup>
-        </>
+        <NewMarkers newLocation={newLocation} viewport={viewport} setNewLocation={setNewLocation} getAllPlaces={getAllPlaces} showNewMarker={showNewMarker}/>
       ) : null}
     </ReactMapGL>
   );
