9f36d5bff1e1f6079a9b2a053b53fa8029ff11c1
arjunsingh <arjunksingh1027@gmail.com>
2021-01-25 14:40:04 -0700

    vertical list works

--- client/src/components/VerticalTabs.js
+++ client/src/components/VerticalTabs.js
@@ -5,6 +5,7 @@ import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
+// import entries from "../pages/Home";
 // import "./tabs.css";
 
 function TabPanel(props) {
@@ -47,7 +48,8 @@ const useStyles = makeStyles((theme) => ({
     borderRight: `1px solid ${theme.palette.divider}`,
   },
 }));
-export default function VerticalTabs() {
+export default function VerticalTabs({ entries }) {
+  console.log(entries);
   const classes = useStyles();
   const [value, setValue] = React.useState(0);
   const handleChange = (event, newValue) => {
@@ -55,43 +57,26 @@ export default function VerticalTabs() {
   };
   return (
     <div className={classes.root}>
-      <Tabs
-        orientation="vertical"
-        variant="scrollable"
-        value={value}
-        onChange={handleChange}
-        aria-label="Vertical tabs example"
-        className={classes.tabs}
-      >
-        <Tab label="Item One" {...a11yProps(0)} />
-        <Tab label="Item Two" {...a11yProps(1)} />
-        <Tab label="Item Three" {...a11yProps(2)} />
-        <Tab label="Item Four" {...a11yProps(3)} />
-        <Tab label="Item Five" {...a11yProps(4)} />
-        <Tab label="Item Six" {...a11yProps(5)} />
-        <Tab label="Item Seven" {...a11yProps(6)} />
-      </Tabs>
-      <TabPanel value={value} index={0}>
-        Item One
-      </TabPanel>
-      <TabPanel value={value} index={1}>
-        Item Two
-      </TabPanel>
-      <TabPanel value={value} index={2}>
-        Item Three
-      </TabPanel>
-      <TabPanel value={value} index={3}>
-        Item Four
-      </TabPanel>
-      <TabPanel value={value} index={4}>
-        Item Five
-      </TabPanel>
-      <TabPanel value={value} index={5}>
-        Item Six
-      </TabPanel>
-      <TabPanel value={value} index={6}>
-        Item Seven
-      </TabPanel>
+      {entries.map((entry) => (
+        <div key={entry._id}>
+          <Tabs
+            orientation="vertical"
+            variant="scrollable"
+            value={value}
+            onChange={handleChange}
+            aria-label="Vertical tabs example"
+            className={classes.tabs}
+          >
+            <Tab
+              {...a11yProps(0)}
+              label={entry.title}
+            />
+          </Tabs>
+          <TabPanel value={value} index={0}>
+            {entry.comments}
+          </TabPanel>
+        </div>
+      ))}
     </div>
   );
 }
--- client/src/pages/SavedList.js
+++ client/src/pages/SavedList.js
@@ -1,13 +1,26 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import ButtonAppBar from "../components/ButtonAppBar";
 import VerticalTabs from "../components/VerticalTabs";
+import entriesList from "../utils/api";
 
 function SavedList() {
+    const [entries, setEntries] = useState([]);
+
+    async function getAllPlaces() {
+        const entries = await entriesList();
+        setEntries(entries);
+      }
+
+    useEffect(() => {
+        getAllPlaces();
+      }, []);
+
+    // console.log(entries);
     return (
         <div>
         <h1 className="apptitle">World Travel App</h1>
             <ButtonAppBar />
-            <VerticalTabs />
+            <VerticalTabs entries={entries}/>
         </div>
     )
 }
